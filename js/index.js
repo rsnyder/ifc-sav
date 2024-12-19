337c34f54ce7ca80948f9029c111f716a8cdc33f
Ron Snyder <ron@snyderjr.com>
2024-12-18 19:37:50 -0500

    testing

--- js/index.js
+++ js/index.js
@@ -253,6 +253,7 @@ const convertTags = (rootEl) => {
     if (parsed.class) iframe.className = parsed.class
     if (parsed.style) applyStyle(iframe, parsed.style)
     iframe.setAttribute('allowfullscreen', '')
+    iframe.setAttribute('allow', 'clipboard-write')
     iframe.src = `${ifcPrefix}/${parsed.tag}?${componentArgs}`
     code.parentElement.replaceWith(iframe)
   })}
